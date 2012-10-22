84fc310dc220bfbcc3c81b71465abd9032d68ec2
Nick Barnes <nickbarnes89@gmail.com>
2012-10-21 23:43:53 -0500

    Accepted 401

--- src/vol1/string/Problem401.java
+++ src/vol1/string/Problem401.java
@@ -1,3 +1,4 @@
+//Palindromes
 package vol1.string;
 
 import java.io.BufferedReader;
@@ -5,17 +6,18 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class Problem401 {
+class Problem401 {
 	public static void main(String[] args) throws Exception {
 		Reader stdin;
 		if (args.length == 1)
-			stdin = new FileReader(args[0] + "/input/vol1/String/Problem401");
+			stdin = new FileReader(args[0] + "/input/vol1/string/Problem401");
 		else
 			stdin = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(stdin);
 		
 		String line = "";
 		while ((line = br.readLine()) != null) {
+			line = line.trim();
 			boolean isPalindrome = checkPalindrome(line);
 			boolean isMirror = checkMirror(line);
 			
@@ -24,7 +26,7 @@ public class Problem401 {
 			else if (isPalindrome)
 				System.out.println(line + " -- is a regular palindrome.");
 			else if (isMirror)
-				System.out.println(line + " -- is a mirror.");
+				System.out.println(line + " -- is a mirrored string.");
 			else
 				System.out.println(line + " -- is not a palindrome.");
 			System.out.println();
@@ -32,9 +34,8 @@ public class Problem401 {
 	}
 	
 	private static boolean checkPalindrome(String line) {
-		for (int i=0; i<line.length()/2-1; i++) {
+		for (int i=0; i<line.length()/2; i++) {
 			char ch1 = line.charAt(i);
-			
 			char ch2 = line.charAt(line.length()-i-1);
 			if (ch1 != ch2)
 				return false;
@@ -43,55 +44,87 @@ public class Problem401 {
 	}
 	
 	private static boolean checkMirror(String line) {
-		for (int i=0; i<line.length()/2-1; i++) {
+		for (int i=0; i<line.length()/2; i++) {
 			char ch1 = line.charAt(i);
 			char ch2 = line.charAt(line.length()-i-1);
 			
-			switch (ch1) {
-			case 'B': case 'C': case 'D': case 'F': case 'G':
-			case 'K': case 'N': case 'P': case 'Q': case 'R':
-			case '4': case '6': case '7': case '9':
+			if (!hasInverse(ch1))
 				return false;
-			case 'A': case 'H': case 'I': case 'M': case 'O':
-			case 'T': case 'U': case 'V': case 'W': case 'X':
-			case 'Y': case '1': case '8':
+			if (isSymmetric(ch1)) {
 				if (ch2 != ch1)
 					return false;
-				break;
-			case 'E':
-				if (ch2 != '3')
-					return false;
-				break;
-			case '3':
-				if (ch2 != 'E')
-					return false;
-				break;
-			case 'J':
-				if (ch2 != 'L')
-					return false;
-				break;
-			case 'L':
-				if (ch2 != 'J')
-					return false;
-				break;
-			case 'S':
-				if (ch2 != '2')
-					return false;
-				break;
-			case '2':
-				if (ch2 != 'S')
-					return false;
-				break;
-			case 'Z':
-				if (ch2 != '5')
-					return false;
-				break;
-			case '5':
-				if (ch2 != 'Z')
-					return false;
-				break;
 			}
+			else if (!isPair(ch1, ch2))
+				return false;
+		}
+		
+		//Check the center character of an odd length pattern
+		if (line.length()%2 == 1) {
+			char center = line.charAt(line.length()/2);
+			if (!isSymmetric(center))
+				return false;
+		}
+		
+		return true;
+	}
+	
+	private static boolean hasInverse(char ch) {
+		switch(ch) {
+		case 'B': case 'C': case 'D': case 'F': case 'G':
+		case 'K': case 'N': case 'P': case 'Q': case 'R':
+		case '4': case '6': case '7': case '9':
+			return false;
 		}
 		return true;
+	}
+	
+	private static boolean isSymmetric(char ch) {
+		switch (ch) {
+		case 'A': case 'H': case 'I': case 'M': case 'O':
+		case 'T': case 'U': case 'V': case 'W': case 'X':
+		case 'Y': case '1': case '8':
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean isPair(char ch1, char ch2) {
+		switch(ch1) {
+		case 'E':
+			if (ch2 == '3')
+				return true;
+			break;
+		case '3':
+			if (ch2 == 'E')
+				return true;
+			break;
+		case 'J':
+			if (ch2 == 'L')
+				return true;
+			break;
+		case 'L':
+			if (ch2 == 'J')
+				return true;
+			break;
+		case 'S':
+			if (ch2 == '2')
+				return true;
+			break;
+		case '2':
+			if (ch2 == 'S')
+				return true;
+			break;
+		case 'Z':
+			if (ch2 == '5')
+				return true;
+			break;
+		case '5':
+			if (ch2 == 'Z')
+				return true;
+			break;
+		default:
+			return false;
+		}
+		return false;
 	}
 }

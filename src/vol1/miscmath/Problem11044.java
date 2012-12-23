68be02108b6c778ce4ff5de199037f5b466e8ce5
Nick Barnes <nickbarnes89@gmail.com>
2012-12-23 11:13:10 -0600

    11044 Searching for Nessy

--- src/vol1/miscmath/Problem11044.java
+++ src/vol1/miscmath/Problem11044.java
@@ -1,4 +1,4 @@
-//Description
+//Searching for Nessy
 package vol1.miscmath;
 
 import java.io.BufferedReader;
@@ -6,12 +6,19 @@ import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class Template {
+public class Problem11044 {
 	static BufferedReader br;
 	
 	public static void main(String[] args) throws Exception {
-		br = getBufferedReader(args, "/input/vol1/miscmath/Problem###");
-		br.readLine();
+		br = getBufferedReader(args, "/input/vol1/miscmath/Problem11044");
+		int tests = Integer.parseInt(br.readLine());
+		for (int t=0; t<tests; t++) {
+			String[] dimensions = br.readLine().trim().split("\\s+");
+			int m = Integer.parseInt(dimensions[0]);
+			int n = Integer.parseInt(dimensions[1]);
+			
+			System.out.println((n/3)*(m/3));
+		}
 	}
 	
 	private static BufferedReader getBufferedReader(String[] args, String path) throws Exception {

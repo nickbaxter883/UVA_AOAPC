a6ee024451b0bc4cfa6492da4699a988a4cd82ef
Nick Barnes <nickbarnes89@gmail.com>
2013-01-04 15:51:06 -0600

    10055 Hashmat the Brave Warrior

--- src/vol0/Problem10055.java
+++ src/vol0/Problem10055.java
@@ -1,17 +1,24 @@
-//Description
-//package vol0;
+//Hashmat the Brave Warrior
+package vol0;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class Template {
+public class Problem10055 {
 	static BufferedReader br;
 	
 	public static void main(String[] args) throws Exception {
-		br = getBufferedReader(args, "/input/vol0/Problem###");
-		br.readLine();
+		br = getBufferedReader(args, "/input/vol0/Problem10055");
+		String line;
+		while ((line = br.readLine()) != null) {
+			String[] armies = line.trim().split("\\s+");
+			long a = Long.parseLong(armies[0]);
+			long b = Long.parseLong(armies[1]);
+			
+			System.out.println(Math.abs(a - b));
+		}
 	}
 	
 	private static BufferedReader getBufferedReader(String[] args, String path) throws Exception {

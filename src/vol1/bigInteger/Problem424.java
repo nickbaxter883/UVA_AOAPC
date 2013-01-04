dabf7745b584c023f1a4ebfa1506897cb2c1cba1
Nick Barnes <nickbarnes89@gmail.com>
2013-01-04 14:04:40 -0600

    424 Integer Inquiry

--- src/vol1/bigInteger/Problem424.java
+++ src/vol1/bigInteger/Problem424.java
@@ -1,17 +1,24 @@
-//Description
-//package vol1.bigInteger;
+//Integer Inquiry
+package vol1.bigInteger;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.math.BigInteger;
 
-public class Template {
+public class Problem424 {
 	static BufferedReader br;
 	
 	public static void main(String[] args) throws Exception {
-		br = getBufferedReader(args, "/input/vol1/bigInteger/Problem###");
-		br.readLine();
+		br = getBufferedReader(args, "/input/vol1/bigInteger/Problem424");
+		String line;
+		BigInteger sum = BigInteger.ZERO;
+		while ((line = br.readLine()) != null) {
+			BigInteger number = new BigInteger(line.trim());
+			sum = sum.add(number);
+		}
+		System.out.println(sum);
 	}
 	
 	private static BufferedReader getBufferedReader(String[] args, String path) throws Exception {
